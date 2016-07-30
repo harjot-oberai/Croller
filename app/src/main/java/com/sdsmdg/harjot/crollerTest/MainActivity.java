7d704ea5b9bd95a71003e24ee1f4f67f877270c8
harjot-oberai <harjot.oberai@gmail.com>
2016-07-31 01:34:07 +0530

    Initial Commit : Basic functionality

---
+++ app/src/main/java/com/sdsmdg/harjot/crollerTest/MainActivity.java
@@ -0,0 +1,20 @@
+package com.sdsmdg.harjot.crollerTest;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    Croller croller;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        croller = (Croller) findViewById(R.id.croller);
+
+        croller.setIndicatorColor(Color.BLUE);
+
+    }
+}
---
+++ croller/src/main/java/com/sdsmdg/harjot/crollerTest/Croller.java
@@ -0,0 +1,263 @@
+package com.sdsmdg.harjot.crollerTest;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by Harjot on 30-Jul-16.
+ */
+public class Croller extends View {
+
+    private static float width, height;
+    private float midx, midy;
+    private Paint textPaint, circlePaint, circlePaint2, linePaint;
+    private String angle;
+    private float currdeg, deg = 3, downdeg, prevCurrDeg;
+    private boolean isIncreasing, isDecreasing;
+
+    private int backCircleColor = Color.parseColor("#222222");
+    private int mainCircleColor = Color.parseColor("#000000");
+    private int indicatorColor = Color.parseColor("#FFA036");
+    private int progressPrimaryColor = Color.parseColor("#FFA036");
+    private int progressSecondaryColor = Color.parseColor("#111111");
+
+    private int progressPrimaryCircleSize;
+    private int progressSecondaryColorSize;
+
+    private int max = 19;
+
+    private float strokeWidth = 7;
+
+    private String label = "Label";
+    private int labelSize = 40;
+    private int labelColor = Color.WHITE;
+
+    private onProgressChangedListener mListener;
+
+    public interface onProgressChangedListener {
+        void onProgressChanged(int progress);
+    }
+
+    public void setOnProgressChangedListener(onProgressChangedListener listener) {
+        mListener = listener;
+    }
+
+    public Croller(Context context) {
+        super(context);
+        init();
+    }
+
+    public Croller(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public Croller(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    void init() {
+        textPaint = new Paint();
+        textPaint.setColor(labelColor);
+        textPaint.setStyle(Paint.Style.FILL);
+        textPaint.setTextSize(labelSize);
+        textPaint.setFakeBoldText(true);
+        textPaint.setTextAlign(Paint.Align.CENTER);
+        circlePaint = new Paint();
+        circlePaint.setColor(progressSecondaryColor);
+        circlePaint.setStyle(Paint.Style.FILL);
+        circlePaint2 = new Paint();
+        circlePaint2.setColor(progressPrimaryColor);
+        circlePaint2.setStyle(Paint.Style.FILL);
+        linePaint = new Paint();
+        linePaint.setColor(indicatorColor);
+        linePaint.setStrokeWidth(strokeWidth);
+        angle = "0.0";
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        midx = canvas.getWidth() / 2;
+        midy = canvas.getHeight() / 2;
+
+        circlePaint.setColor(progressSecondaryColor);
+        circlePaint2.setColor(progressPrimaryColor);
+        linePaint.setStrokeWidth(strokeWidth);
+        linePaint.setColor(indicatorColor);
+        textPaint.setColor(labelColor);
+        textPaint.setTextSize(labelSize);
+
+        int ang = 0;
+        float x = 0, y = 0;
+        int radius = (int) (Math.min(midx, midy) * ((float) 14.5 / 16));
+        float deg2 = Math.max(3, deg);
+        float deg3 = Math.min(deg, 21);
+        for (int i = (int) (deg2); i < 22; i++) {
+            float tmp = (float) i / 24;
+            x = midx + (float) (radius * Math.sin(2 * Math.PI * (1.0 - tmp)));
+            y = midy + (float) (radius * Math.cos(2 * Math.PI * (1.0 - tmp)));
+            circlePaint.setColor(progressSecondaryColor);
+            canvas.drawCircle(x, y, ((float) radius / 15), circlePaint);
+        }
+        for (int i = 3; i <= deg3; i++) {
+            float tmp = (float) i / 24;
+            x = midx + (float) (radius * Math.sin(2 * Math.PI * (1.0 - tmp)));
+            y = midy + (float) (radius * Math.cos(2 * Math.PI * (1.0 - tmp)));
+            canvas.drawCircle(x, y, ((float) radius / 15), circlePaint2);
+        }
+
+        float tmp2 = (float) deg / 24;
+        float x1 = midx + (float) (radius * ((float) 2 / 5) * Math.sin(2 * Math.PI * (1.0 - tmp2)));
+        float y1 = midy + (float) (radius * ((float) 2 / 5) * Math.cos(2 * Math.PI * (1.0 - tmp2)));
+        float x2 = midx + (float) (radius * ((float) 3 / 5) * Math.sin(2 * Math.PI * (1.0 - tmp2)));
+        float y2 = midy + (float) (radius * ((float) 3 / 5) * Math.cos(2 * Math.PI * (1.0 - tmp2)));
+
+        circlePaint.setColor(backCircleColor);
+        canvas.drawCircle(midx, midy, radius * ((float) 13 / 15), circlePaint);
+        circlePaint.setColor(mainCircleColor);
+        canvas.drawCircle(midx, midy, radius * ((float) 11 / 15), circlePaint);
+        canvas.drawText(label, midx, midy + (float) (radius * 1.1), textPaint);
+        canvas.drawText(String.valueOf(getProgress()), midx, midy, textPaint);
+        canvas.drawLine(x1, y1, x2, y2, linePaint);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+
+        if (mListener != null)
+            mListener.onProgressChanged((int) (deg - 2));
+
+        if (e.getAction() == MotionEvent.ACTION_DOWN) {
+            float dx = e.getX() - midx;
+            float dy = e.getY() - midy;
+            downdeg = (float) ((Math.atan2(dy, dx) * 180) / Math.PI);
+            downdeg -= 90;
+            if (downdeg < 0) {
+                downdeg += 360;
+            }
+            downdeg = (float) Math.floor(downdeg / 15);
+            return true;
+        }
+        if (e.getAction() == MotionEvent.ACTION_MOVE) {
+            float dx = e.getX() - midx;
+            float dy = e.getY() - midy;
+            currdeg = (float) ((Math.atan2(dy, dx) * 180) / Math.PI);
+            currdeg -= 90;
+            if (currdeg < 0) {
+                currdeg += 360;
+            }
+            currdeg = (float) Math.floor(currdeg / 15);
+
+            if (currdeg == 0 && downdeg == 23) {
+                deg++;
+                if (deg > 21) {
+                    deg = 21;
+                }
+                downdeg = currdeg;
+            } else if (currdeg == 23 && downdeg == 0) {
+                deg--;
+                if (deg < 3) {
+                    deg = 3;
+                }
+                downdeg = currdeg;
+            } else {
+                deg += (currdeg - downdeg);
+                if (deg > 21) {
+                    deg = 21;
+                }
+                if (deg < 3) {
+                    deg = 3;
+                }
+                downdeg = currdeg;
+            }
+
+            angle = String.valueOf(String.valueOf(deg));
+            invalidate();
+            return true;
+        }
+        if (e.getAction() == MotionEvent.ACTION_UP) {
+            return true;
+        }
+        return super.onTouchEvent(e);
+    }
+
+    public int getProgress() {
+        return (int) (deg - 2);
+    }
+
+    public void setProgress(int x) {
+        deg = x + 2;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String txt) {
+        label = txt;
+    }
+
+    public int getBackCircleColor() {
+        return backCircleColor;
+    }
+
+    public void setBackCircleColor(int backCircleColor) {
+        this.backCircleColor = backCircleColor;
+    }
+
+    public int getMainCircleColor() {
+        return mainCircleColor;
+    }
+
+    public void setMainCircleColor(int mainCircleColor) {
+        this.mainCircleColor = mainCircleColor;
+    }
+
+    public int getIndicatorColor() {
+        return indicatorColor;
+    }
+
+    public void setIndicatorColor(int indicatorColor) {
+        this.indicatorColor = indicatorColor;
+    }
+
+    public int getProgressPrimaryColor() {
+        return progressPrimaryColor;
+    }
+
+    public void setProgressPrimaryColor(int progressPrimaryColor) {
+        this.progressPrimaryColor = progressPrimaryColor;
+    }
+
+    public int getProgressSecondaryColor() {
+        return progressSecondaryColor;
+    }
+
+    public void setProgressSecondaryColor(int progressSecondaryColor) {
+        this.progressSecondaryColor = progressSecondaryColor;
+    }
+
+    public int getLabelSize() {
+        return labelSize;
+    }
+
+    public void setLabelSize(int labelSize) {
+        this.labelSize = labelSize;
+    }
+
+    public int getLabelColor() {
+        return labelColor;
+    }
+
+    public void setLabelColor(int labelColor) {
+        this.labelColor = labelColor;
+    }
+}
