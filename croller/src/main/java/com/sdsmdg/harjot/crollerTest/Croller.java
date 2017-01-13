ee7a2ddd7a72b3d6d1fa5a46f5d35509dd730a2d
harjot-oberai <harjot.oberai@gmail.com>
2017-01-14 00:48:33 +0530

    Updated for bintray upload

--- croller/src/main/java/com/sdsmdg/harjot/crollerTest/Croller.java
+++ croller/src/main/java/com/sdsmdg/harjot/crollerTest/Croller.java
@@ -222,7 +222,7 @@ public class Croller extends View {
             circlePaint.setColor(mainCircleColor);
             canvas.drawCircle(midx, midy, mainCircleRadius, circlePaint);
             canvas.drawText(label, midx, midy + (float) (radius * 1.1), textPaint);
-            canvas.drawText(String.valueOf(getProgress()), midx, midy, textPaint);
+//            canvas.drawText(String.valueOf(getProgress()), midx, midy, textPaint);
             canvas.drawLine(x1, y1, x2, y2, linePaint);
 
         } else {
@@ -274,7 +274,7 @@ public class Croller extends View {
             circlePaint.setColor(mainCircleColor);
             canvas.drawCircle(midx, midy, mainCircleRadius, circlePaint);
             canvas.drawText(label, midx, midy + (float) (radius * 1.1), textPaint);
-            canvas.drawText(String.valueOf(getProgress()), midx, midy, textPaint);
+//            canvas.drawText(String.valueOf(getProgress()), midx, midy, textPaint);
             canvas.drawLine(x1, y1, x2, y2, linePaint);
         }
 
