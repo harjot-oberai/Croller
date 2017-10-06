7b9d971127f067f5a9fda770ae4b8dd131fdf130
harjot-oberai <harjot.oberai@gmail.com>
2017-10-07 02:53:43 +0530

    Updated to v1.0.6

--- app/src/main/java/com/sdsmdg/harjot/crollerTest/MainActivity.java
+++ app/src/main/java/com/sdsmdg/harjot/crollerTest/MainActivity.java
@@ -1,8 +1,8 @@
 package com.sdsmdg.harjot.crollerTest;
 
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        croller = (Croller) findViewById(R.id.croller);
 
+        croller = (Croller) findViewById(R.id.croller);
 //        croller.setIndicatorWidth(10);
 //        croller.setBackCircleColor(Color.parseColor("#EDEDED"));
 //        croller.setMainCircleColor(Color.WHITE);
@@ -26,5 +26,22 @@ public class MainActivity extends AppCompatActivity {
 //        croller.setProgressSecondaryColor(Color.parseColor("#EEEEEE"));
 //        croller.setProgressRadius(380);
 //        croller.setBackCircleRadius(300);
+
+        croller.setOnCrollerChangeListener(new OnCrollerChangeListener() {
+            @Override
+            public void onProgressChanged(Croller croller, int progress) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(Croller croller) {
+                Toast.makeText(MainActivity.this, "Start", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(Croller croller) {
+                Toast.makeText(MainActivity.this, "Stop", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
